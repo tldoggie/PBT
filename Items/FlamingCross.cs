86277023058137f9b341d1aba261a6d8dda8c092
tldoggie <85531418+tldoggie@users.noreply.github.com>
2023-06-04 18:56:51 -0500

    kanye north
    These changes are IMPORTANT!

---
+++ Items/FlamingCross.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PBT.Items
+{
+    public class FlamingCross : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ash's Flaming Cross"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+            Tooltip.SetDefault("Don't ask him what he was using it for");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 35;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 20;
+            Item.height = 20;
+            Item.scale = 2;
+            Item.useTime = 75;
+            Item.useAnimation = 75;
+            Item.useStyle = 1;
+            Item.knockBack = 15;
+            Item.value = 6000;
+            Item.rare = 2;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+        }
+
+        public override void HoldItem(Player player)
+        {
+            Vector2 position = player.RotatedRelativePoint(new Vector2(player.itemLocation.X + 12f * player.direction + player.velocity.X, player.itemLocation.Y - 14f + player.velocity.Y), true);
+            Lighting.AddLight(position, 1f, 1f, 1f);
+        }
+
+        public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
+        {
+            target.AddBuff(BuffID.OnFire, 360);
+            base.OnHitNPC(player, target, damage, knockBack, crit);
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ModContent.ItemType<WoodenCross>(), 1);
+            recipe.AddIngredient(ItemID.Torch, 20);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+}
---
+++ Items/WoodenCross.cs
@@ -0,0 +1,41 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PBT.Items
+{
+    public class WoodenCross : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ash's Wooden Cross"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+            Tooltip.SetDefault("The rusty nails add an extra punch");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 30;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 20;
+            Item.height = 20;
+            Item.scale = 2;
+            Item.useTime = 75;
+            Item.useAnimation = 75;
+            Item.useStyle = 1;
+            Item.knockBack = 15;
+            Item.value = 5000;
+            Item.rare = 1;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Wood, 20);
+            recipe.AddRecipeGroup(RecipeGroupID.IronBar, 2);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+}
