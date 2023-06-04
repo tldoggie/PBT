f8917267ca0b3cd201d1d27311a5e99e878b8005
MHTal <adam0223chess@outlook.com>
2023-06-04 18:20:29 -0500

    frisbee

---
+++ Items/frisbee.cs
@@ -0,0 +1,44 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using PBT.Projectiles;
+namespace PBT.Items
+{
+	public class frisbee : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Wooden Frisbee"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("lock in");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.damage = 40;
+			Item.DamageType = DamageClass.Ranged;
+			Item.width = 10;
+			Item.height = 10;
+			Item.useTime = 12;
+			Item.useAnimation = 12;
+			Item.useStyle = 1;
+			Item.knockBack = 3;
+			Item.value = 1000;
+			Item.rare = ItemRarityID.Green;
+			Item.UseSound = SoundID.Item7;
+			Item.shoot = ModContent.ProjectileType<frisbeeprojectile>();
+			Item.shootSpeed = 25;
+			Item.accessory = false;
+			Item.autoReuse = false;
+			Item.crit = 12;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			// recipe.AddIngredient(ItemID.PinkGel, 2);
+            recipe.AddIngredient(ItemID.Wood, 15);
+            recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
+		}
+	}
+}
---
+++ Projectiles/frisbeeprojectile.cs
@@ -0,0 +1,47 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+namespace PBT.Projectiles
+{
+	public class frisbeeprojectile : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Frisbee Projectile");
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 20;
+			Projectile.height = 100;
+			Projectile.aiStyle = ProjAIStyleID.Boomerang;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Ranged;
+			Projectile.maxPenetrate = 3;
+			AIType = ProjectileID.WoodenBoomerang;
+		}
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+				Collision.HitTiles(Projectile.position, Projectile.velocity, Projectile.width, Projectile.height);
+
+				// If the projectile hits the left or right side of the tile, reverse the X velocity
+				if (Math.Abs(Projectile.velocity.X - oldVelocity.X) > float.Epsilon)
+				{
+					Projectile.velocity.X = -oldVelocity.X;
+				}
+
+				// If the projectile hits the top or bottom side of the tile, reverse the Y velocity
+				if (Math.Abs(Projectile.velocity.Y - oldVelocity.Y) > float.Epsilon)
+				{
+					Projectile.velocity.Y = -oldVelocity.Y;
+				}
+			return false;
+			
+		}
+
+		// Additional hooks/methods here.
+	}
+}
